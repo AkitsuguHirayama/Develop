298939c998baefe91875aeb9bc3d93788c376a59
”AkitsuguHirayama” <a.hirayama1027@outlook.jp>
2021-03-23 07:34:44 +0000

    20210323

--- db/migrate/20210320131901_users.rb
+++ db/migrate/20210320131901_users.rb
@@ -3,8 +3,8 @@ class CreateUsers < ActiveRecord::Migration[5.2]
     create_table :users do |t|
       t.string :name
       t.string :email
-
-      t.timestamps
+      t.string :pregnantday
+      t.string :birthday
     end
   end
 end
